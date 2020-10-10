1d4aab0e985e94357f42836f8edd34d99fa40303
unchainedhank <2450312896@qq.com>
2020-10-10 21:59:48 +0800

    LinkedListCycle solved.

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,30 @@
+public class LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+}
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode fast = head.next;
+        ListNode slow = head;
+
+        while (fast != slow) {
+
+            if (fast == null || fast.next == null)
+                return false;
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+        }
+        return true;
+    }
+}
