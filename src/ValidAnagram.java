526089bcfb6c2b452099da0b865407e4fdb7add8
unchainedhank <2450312896@qq.com>
2020-10-17 09:37:58 +0800

    242 valid-anagram my solution built.

--- src/ValidAnagram.java
+++ src/ValidAnagram.java
@@ -18,4 +18,41 @@ public class ValidAnagram {
         }
         return true;
     }
+
+    public static boolean isAnagram2(String s, String t) {
+        if (s.equals(t))
+            return true;
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        var map = new HashMap<Character,Integer>();
+
+        //统计s的词频
+        for (int i = 0; i < s.length(); i++) {
+            Character cs = s.charAt(i);
+
+            if (!map.containsKey(cs)) {
+                map.put(cs,1);
+            }
+            else {
+                map.put(cs, map.get(cs) + 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            Character ct = t.charAt(i);
+            if (!map.containsKey(ct))
+                return false;
+            else
+                map.put(ct,map.get(ct) - 1);
+                if (map.get(ct) < 0)
+                    return false;
+        }
+        return true;
+    }
+
+
+
 }
