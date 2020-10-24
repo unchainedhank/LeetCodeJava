ae860d1520680f9356a5c1295e254158c30fa329
unchainedhank <2450312896@qq.com>
2020-10-24 21:56:17 +0800

    problem 328 odd-even-linked-list solved.

--- src/OddEvenLinkedList.java
+++ src/OddEvenLinkedList.java
@@ -1,2 +1,49 @@
+/**
+ * 给定一个单链表，把所有的奇数节点和偶数节点分别排在一起。请注意，这里的奇数节点和偶数节点指的是节点编号的奇偶性，而不是节点的值的奇偶性。
+ *
+ * 请尝试使用原地算法完成。你的算法的空间复杂度应为 O(1)，时间复杂度应为 O(nodes)，nodes 为节点总数。
+ *
+ * 示例 1:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 1->3->5->2->4->NULL
+ * 示例 2:
+ *
+ * 输入: 2->1->3->5->6->4->7->NULL
+ * 输出: 2->3->6->7->1->5->4->NULL
+ */
 public class OddEvenLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null)
+            return null;
+        /**
+         * odd是第一个奇数节点的指针，even和evenHead是第一个偶数节点的指针
+         * 1.odd块指向下一个奇数，odd往后移
+         * 2.even块指向下一个偶数，even往后移
+         * 3.最后奇数的next就是evenHead
+         */
+        ListNode odd = head, even = odd.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
 }
