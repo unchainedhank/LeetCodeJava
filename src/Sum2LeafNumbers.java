623ffab4abad18db2b281164b64e475b6b80f761
unchainedhank <2450312896@qq.com>
2020-10-29 10:15:28 +0800

    problem 129 sum-root-to-leaf-numbers solved.

--- src/Sum2LeafNumbers.java
+++ src/Sum2LeafNumbers.java
@@ -1,2 +1,54 @@
+/**
+ * 例如，从根到叶子节点路径 1->2->3 代表数字 123。
+ *
+ * 计算从根到叶子节点生成的所有数字之和。
+ *
+ * 说明:叶子节点是指没有子节点的节点。
+ *
+ * 示例 1:
+ *
+ * 输入: [1,2,3]
+ *     1
+ *    / \
+ *   2   3
+ * 输出: 25
+ * 解释:
+ * 从根到叶子节点路径 1->2 代表数字 12.
+ * 从根到叶子节点路径 1->3 代表数字 13.
+ * 因此，数字总和 = 12 + 13 = 25.
+ * 示例 2:
+ *
+ * 输入: [4,9,0,5,1]
+ *     4
+ *    / \
+ *   9   0
+ *  / \
+ * 5   1
+ * 输出: 1026
+ * 解释:
+ * 从根到叶子节点路径 4->9->5 代表数字 495.
+ * 从根到叶子节点路径 4->9->1 代表数字 491.
+ * 从根到叶子节点路径 4->0 代表数字 40.
+ * 因此，数字总和 = 495 + 491 + 40 = 1026.
+ */
 public class Sum2LeafNumbers {
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+
+    //上一层的值*10传到这层+val，再传到下层又会被*10
+    public int helper(TreeNode root, int i){
+        if (root == null) return 0;
+        int temp = i * 10 + root.val;
+        if (root.left == null && root.right == null)
+            return temp;
+        return helper(root.left, temp) + helper(root.right, temp);
+    }
+
 }
