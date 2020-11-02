77730080f85418d1ca03c75665ea804cc23578b7
unchainedhank <2450312896@qq.com>
2020-11-02 22:00:33 +0800

    problem 237 delete-node-in-a-linked-list solved.

--- src/DeleteNodeLinkedList.java
+++ src/DeleteNodeLinkedList.java
@@ -1,3 +1,14 @@
 public class DeleteNodeLinkedList {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            this.val = x;
+        }
+    }
 
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
 }
